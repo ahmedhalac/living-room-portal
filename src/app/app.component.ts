0853c217ffd950a9d81a3e0f9771f49d6ad45c1a
ahmedhalac <ahmedhalac@outlook.com>
2025-02-19 01:25:18 +0100

    added sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { OverviewSectionComponent } from './components/overview-section/overview
 import { AreasItemsComponent } from './components/areas-items/areas-items.component';
 import { ClientInfoComponent } from './components/client-info/client-info.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, ClientInfoComponent, OverviewSectionComponent, AreasItemsComponent, HeaderComponent],
+  imports: [CommonModule, ClientInfoComponent, OverviewSectionComponent, AreasItemsComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
